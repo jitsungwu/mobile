fb0f189f7ca3710bbf9ef6207f1a19d431b012a9
Ben Wu <jitsung.wu@gmail.com>
2021-10-19 22:36:45 +0800

    fo 10/19

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
